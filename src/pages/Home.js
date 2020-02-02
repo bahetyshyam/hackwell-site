0fae4912b640b72f2ded30a023987b9be9bd1a8f
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-02-03 00:11:32 +0530

    Created home page

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+import Faq from '../sections/Faq';
+import About from '../sections/About';
+import Landing from '../sections/Landing';
+import Rules from '../sections/Rules';
+import Social from '../sections/Social';
+import Sponsors from '../sections/Sponsors';
+import Tracks from '../sections/Tracks';
+
+const Home = () => {
+  return (
+    <>
+      <Landing />
+      <Header home={true} />
+      <main className='mt-6'>
+        <About />
+        <Tracks />
+        <Rules />
+        <Sponsors />
+        <Faq />
+        <Social />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
