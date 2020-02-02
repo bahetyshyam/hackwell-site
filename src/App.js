4bec38baa317eba001dabb6bb887b297e6c5ffeb
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-02-03 00:56:58 +0530

    Routes added to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import CodeOfConduct from './pages/CodeOfConduct';
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 const App = () => {
-  return <div>Hello, friend.</div>;
+  return (
+    <Switch>
+      <Route path='/register' exact component={Register} />
+      <Route path='/code-of-conduct' exact component={CodeOfConduct} />
+      <Route path='/' exact component={Home} />
+    </Switch>
+  );
 };
 
 export default App;
