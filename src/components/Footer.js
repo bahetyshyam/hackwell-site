2b65bb9d34ddf7ea75cef5d883a9ede10d9f1a8e
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-02-08 19:13:45 +0530

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,32 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className='pt-32'>
-      <div className='p-4 bg-blue-700'>
-        <div className='container flex items-center justify-between'>
-          <Link
-            className='text-gray-200 hover:text-white'
-            to='/code-of-conduct'
-          >
-            Code of Conduct
-          </Link>
-          <a className='text-gray-200 hover:text-white' href='/'>
-            <img src='/img/hackwell-logo-white.png' alt="Hackwell's logo" />
-          </a>
-          <a
-            className='text-gray-200 hover:text-white'
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://github.com/sidsbrmnn/hackwell-site'
-          >
-            &lt;/&gt; with &#9829;
-          </a>
+    <footer className='px-4 pt-32'>
+      <div className='bg-blue-700 -mx-4'>
+        <div className='container flex flex-col justify-between items-center md:flex-row'>
+          <div className='py-4'>
+            <Link
+              className='text-gray-200 hover:text-white'
+              to='/code-of-conduct'
+            >
+              Code of Conduct
+            </Link>
+          </div>
+          <div className='py-4'>
+            <a className='text-gray-200 hover:text-white' href='/'>
+              <img src='/img/hackwell-logo-white.png' alt="Hackwell's logo" />
+            </a>
+          </div>
+          <div className='py-4'>
+            <a
+              className='text-gray-200 hover:text-white'
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://github.com/sidsbrmnn/hackwell-site'
+            >
+              &lt;/&gt; with &#9829;
+            </a>
+          </div>
         </div>
       </div>
     </footer>
