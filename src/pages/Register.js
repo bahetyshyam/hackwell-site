39def6668ae40f4458f305dcfc1cfb9c0c487fcd
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-02-08 19:14:59 +0530

    Removed unused empty tag

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,28 +4,25 @@ import Form from '../components/Form';
 
 const Register = () => {
   return (
-    <>
-      <main>
-        <div className='lg:flex'>
-          <div className='lg:w-1/2 lg:h-screen lg:top-0 lg:sticky'>
-            <div className='px-8 py-10'>
-              <a className='inline-block' href='/'>
-                <img src='/images/hackwell-logo.png' alt="Hackwell's logo" />
-              </a>
-            </div>
-          </div>
-          <div className='lg:w-1/2'>
-            <div className='px-8 py-10'>
-              <h2 className='text-4xl md:text-5xl leading-tight font-medium text-gray-900'>
-                <span className='font-bold text-blue-600'>Register</span> to
-                hack
-              </h2>
-              <Form />
-            </div>
+    <main>
+      <div className='lg:flex'>
+        <div className='lg:w-1/2 lg:h-screen lg:top-0 lg:sticky'>
+          <div className='px-8 py-10'>
+            <a className='inline-block' href='/'>
+              <img src='/images/hackwell-logo.png' alt="Hackwell's logo" />
+            </a>
           </div>
         </div>
-      </main>
-    </>
+        <div className='lg:w-1/2'>
+          <div className='px-8 py-10'>
+            <h2 className='text-4xl md:text-5xl leading-tight font-medium text-gray-900'>
+              <span className='font-bold text-blue-600'>Register</span> to hack
+            </h2>
+            <Form />
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
