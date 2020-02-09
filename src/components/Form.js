67270c0d8664e44956c2f64f792341a5cf2030cf
Shyam Bahety <bahetyshyam@gmail.com>
2020-02-09 19:27:19 +0530

    Added Register Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,74 @@ class Form extends Component {
 
   render() {
     return (
-      <form>
-        <div>
-          <label>What is your team size?</label>
-          <input type='number' name='size' id='size' value='1' min='1' />
-        </div>
-      </form>
+      <div className="w-full pt-5">
+        <form className="bg-white shadow rounded">
+
+          {/* No of members div */}
+          <div className="px-6 py-2">
+            <label className="block text-black mb-2">
+              No. of Members
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-2 mb-2 leading-tight focus:outline-none focus:shadow-outline" type="number" min="1" max="4" placeholder="No. of members"></input>
+            <p class="text-red-500 text-xs italic">Error Message</p>
+          </div>
+
+          {/* College Name and Team Name Flex Box */}
+          <div className="flex flex-wrap">
+            <div className="w-full md:w-1/2 px-6 py-2">
+              <label className="block text-black mb-2">
+                College Name
+            </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-2 mb-2 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="College Name"></input>
+              <p class="text-red-500 text-xs italic">Error Message</p>
+            </div>
+            <div className="w-full md:w-1/2 px-6 py-2">
+              <label className="block text-black mb-2">
+                Team Name
+            </label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-2 mb-2 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Team Name"></input>
+              <p class="text-red-500 text-xs italic">Error Message</p>
+            </div>
+          </div>
+
+          {/* Member Details Section Starts Here */}
+          <p className="px-6 mt-6 text-black font-bold">Member Details</p>
+
+          {/* Member Name */}
+          <div className="px-6 py-2">
+            <label className="block text-black mb-2">
+              Name
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-2 mb-2 leading-tight focus:outline-none focus:shadow-outline " type="text" placeholder="Name"></input>
+            <p class="text-red-500 text-xs italic">Error Message</p>
+          </div>
+
+          {/* Member Phone Number */}
+          <div className="px-6 py-2">
+            <label className="block text-black mb-2">
+              Phone
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-2 mb-2 leading-tight focus:outline-none focus:shadow-outline " type="tel" placeholder="Phone"></input>
+            <p class="text-red-500 text-xs italic">Error Message</p>
+          </div>
+
+          {/* Member Email */}
+          <div className="px-6 py-2">
+            <label className="block text-black mb-2">
+              Email
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-2 mb-2 leading-tight focus:outline-none focus:shadow-outline " type="email" placeholder="Email"></input>
+            <p class="text-red-500 text-xs italic">Error Message</p>
+          </div>
+
+          {/* Register Button */}
+          <div className="flex justify-center px-6 pt-2 pb-5">
+            <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Register
+             </button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
